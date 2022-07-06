c7dc80e737d15d8c2d651aa7364908a9acf59e3a
Suheb Ali <ersuheb@gmail.com>
2022-07-06 22:35:02 +0530

    1234 1234 1234 1234

---
+++ C++/Patterns/Pattern_3.cpp
@@ -0,0 +1,36 @@
+/*
+
+1234
+1234
+1234
+1234
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int row = 1;
+
+    while(row<=n){
+
+        int col = 1;
+        while(col<=n){
+            cout << col << " ";
+            col++;
+        }
+
+        cout << endl;
+        row++;
+
+    }
+
+    return 0;
+
+}
