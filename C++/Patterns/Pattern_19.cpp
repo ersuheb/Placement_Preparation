90db84d12e743a36efa6d469d369e453bc6a8b08
Suheb Ali <ersuheb@gmail.com>
2022-07-07 00:32:46 +0530

    D CD BCD ABCD

---
+++ C++/Patterns/Pattern_19.cpp
@@ -0,0 +1,36 @@
+/*
+
+D
+CD
+BCD
+ABCD
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+        
+        int n;
+        cin >> n;
+        
+        int row = 1;
+        
+        while(row<=n){
+            
+            int col = 1;
+            char ch = 'A' + n - row;
+            
+            while(col<=row){
+                cout << ch << " ";
+                ch++;
+                col++;
+            }
+            
+            cout << endl;
+            row++;
+            
+        }
+        return 0;
+}
