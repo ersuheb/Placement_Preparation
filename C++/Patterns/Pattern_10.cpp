0258019acc3a63be860de2cfccbbaca5ac566431
Suheb Ali <ersuheb@gmail.com>
2022-07-06 23:51:41 +0530

    without using extra variable

--- C++/Patterns/Pattern_10.cpp
+++ C++/Patterns/Pattern_10.cpp
@@ -8,6 +8,8 @@
 */
 
 
+/* With using extra variable
+
 #include <iostream>
 using namespace std;
 
@@ -36,3 +38,33 @@ int main(){
         }
         
 }
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+        
+        int n;
+        cin >> n;
+        
+        int row = 1;
+        
+        while(row<=n){
+            
+            int col = row;
+
+            while(col < row * 2){
+                cout << col << " ";
+                col++;
+            }
+            
+            cout << endl;
+            row++;
+            
+        }
+        
+}
+
