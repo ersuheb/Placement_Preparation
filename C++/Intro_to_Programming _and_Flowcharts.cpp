5d4f7a110d404a9e94f39ee16b9e6af11051234c
Suheb Ali <ersuheb@gmail.com>
2022-07-06 16:58:21 +0530

    init

---
+++ C++/Intro_to_Programming _and_Flowcharts.cpp
@@ -0,0 +1,75 @@
+/*
+
+How to solve a programming problem?
+1. Understand the problem. 
+2. Draw a flowchart. (Check the given values)
+3. Write pseudocode. (Figure out an approach)
+4. Write code. 
+5. Test code.
+
+Given some problem P, say you 'think' of some solution, ki aise aise krenge, etc. Now write down this crude solution on paper, 
+not necessarily in correct syntax (code ki bhasha). Now your idea is on paper. Convert this rough work, also called 'pseudocode'
+into a program in a programming lang. usage of your choice, say C++.
+
+Pseudocode: A very simple and high-level (upar-upar ka) form of computer langage that is used in program design.
+
+A Flowchart is diagramatic representation of an approach. This draws out all the steps of your approach in order.
+
+Components of a flowchart:
+    1. Terminator: Specifies the start and end of the flowchart.
+    2. Parallelogram:For taking input or showing output.
+    3. Process: Operations and processes ke liye.
+    4. Decision: Decision making.
+    5. Circle: Connectors.
+    6. Arrow: Connects the components.
+
+
+Pseudocode for adding 2 numbers:
+
+    input 2 numbers a and b
+    let sum = a + b
+    print out sum
+    
+    or
+
+    read a and b
+    sum variable is a+b
+    sum chaapo
+
+Both pseudocodes are ok. No pseudocode is wrong as long as the logic is same/similar.
+
+What is a Programming Language?
+
+    Jaise khana khane ke liye mummy/papa ko bolna padhta hai, vaise hi we must instruct our computer to perform some task for us.
+
+    More formally, a programming language is a way to communicate with a computer.
+    It is a formal language which consist of set of strings that produce various kinds of machine output.
+
+    E.g. C++, Java, Python, C#, etc.
+
+    C++ program to display "Hello World"
+ 
+    //Header file for input output functions
+    #include <iostream>
+    using namespace std;
+ 
+    //Main() function: where the execution of program begins
+    int main()
+    {
+        //prints hello world
+        cout << "Hello World";
+        return 0;
+    }
+    
+    Every language must be written following some rules called syntax of that language.
+
+    A computer essentially only understands binary codes of 0s and 1s.
+    A compiler processes the statements of a programming language into Machine Code (Binary).
+
+    Source Code --> Compiler --> Machine Code --> Executable Code (.exe)
+    
+    Read more about Compilers here: https://www.geeksforgeeks.org/introduction-to-compilers/
+
+*/ 
+
+
