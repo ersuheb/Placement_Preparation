c37fb537fbb5669eb5d2f82bcc5fe25ee6dc0bce
Suheb Ali <ersuheb@gmail.com>
2022-07-06 23:37:27 +0530

    1 22 333 4444

---
+++ C++/Patterns/Pattern_8.cpp
@@ -0,0 +1,35 @@
+/*
+
+1
+22
+333
+4444
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    
+    int row = 1;
+    
+    while(row<=n){
+        int col = 1;
+        while(col<=row){
+            cout << row << " ";
+            col++;
+        }
+        
+        cout << endl;
+        row++;
+        
+    }
+    
+    return 0;
+    
+
+}
