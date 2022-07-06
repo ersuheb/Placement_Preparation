5782456c36dda9d76e5edbccc504b6a2eaaf45f3
Suheb Ali <ersuheb@gmail.com>
2022-07-07 00:22:27 +0530

    A BC DEF GHIJ

---
+++ C++/Patterns/Pattern_17.cpp
@@ -0,0 +1,36 @@
+/*
+
+A
+BC
+DEF
+GHIJ
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    
+    int row = 1;
+    char ch = 'A';
+    
+    while(row<=n){
+        
+        int col = 1;
+        
+        while(col<=row){
+            cout << ch << " ";
+            ch++;
+            col++;
+        }
+        
+        cout << endl;
+        row++;
+        
+    }
+    return 0;
+}
