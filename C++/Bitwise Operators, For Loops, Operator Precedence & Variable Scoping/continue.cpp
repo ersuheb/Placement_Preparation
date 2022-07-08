2bce525e6558f1255f1f7523ffc7a46b507b1614
Suheb Ali <ersuheb@gmail.com>
2022-07-08 07:48:18 +0530

    Bitwise Operators, For Loops, Operator Preced.....

---
+++ C++/Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/continue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    for(int i=0; i<5; i++) {
+
+        cout<< " HI " << endl;
+        cout<< " Hey " << endl;
+        continue;
+
+        cout<< "Reply krde bhai " <<endl;
+
+    }
+    return 0;
+}
