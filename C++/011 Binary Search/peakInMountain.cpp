3bea8d01a62408a8b6c88cc41b6fc7b7049cde54
Suheb Ali <ersuheb@gmail.com>
2022-07-14 00:48:20 +0530

    binarySearch

--- C++/011 Binary Search/peakInMountain.cpp
+++ C++/011 Binary Search/peakInMountain.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 class Solution {
 public:
-  
-    int find_pivot(vector<int> v) {
-	int s = 0, e = v.size() - 1;
-	int mid=(s + e) / 2;
-	while (s < e)
-	{
-		if(v[mid] < v[mid+1])
-     			s=mid+1;
-    		else
-      			e = mid;
-    
-		mid = (s + e) / 2;
-	}
-	return s;
-    }
     int peakIndexInMountainArray(vector<int>& arr) {
-        return find_pivot(arr);
+        int s = 0;
+        int e = arr.size() - 1;
+        
+        int mid = s + (e-s)/2;
+        
+        while(s<e) {
+            if(arr[mid] < arr[mid+1]){
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid;
+            }
+            mid = s  + (e-s)/2;
+        }
+        return s;
     }
 };
